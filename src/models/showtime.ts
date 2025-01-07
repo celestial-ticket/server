e9cbd4d1e381c57cb3a62d1cf5722d013f3aafe7
nrlizahh <nurhalizah.sn101@gmail.com>
2025-01-07 09:46:27 +0700

    fix: showTimeSchema

--- src/models/showtime.ts
+++ src/models/showtime.ts
@@ -7,26 +7,28 @@ export default class ShowTime {
 
   // Query Get Show Time (Aggregate), Match by FilmID, Date, Group By CinemaID, Lookup ke Cinema By CinemaID
   static async findAll(movieId: ObjectId, date: string): Promise<IShowTime[]> {
-    // (Aggregate), Match by FilmID, Date, Group By CinemaID, Lookup ke Cinema By CinemaID
-    console.log(movieId, date, "????, showtime model 1");
+    console.log(`Fetching show times for movieId: ${movieId}, date: ${date}`);
+
+    // Convert date to start and end of day
+    const startDate = new Date(date);
+    const endDate = new Date(date);
+    endDate.setDate(startDate.getDate() + 1);
+
+    console.log(movieId, startDate, endDate);
+
+    // Aggregate pipeline
     const pipeline = await this.coll
       .aggregate([
         {
-            $match: {
-                movie: movieId,
-                date: new Date(date),
-            },
-        },
-        {
-          $group: {
-            _id: "$cinema",
-            showTimes: { $push: "$$ROOT" },
+          $match: {
+            movieId: movieId,
+            date: { $gte: startDate, $lt: endDate },
           },
         },
         {
           $lookup: {
             from: "cinemas",
-            localField: "_id",
+            localField: "cinemaId",
             foreignField: "_id",
             as: "cinema",
           },
@@ -35,16 +37,21 @@ export default class ShowTime {
           $unwind: "$cinema",
         },
         {
-          $project: {
-            _id: 0,
-            cinema: 1,
-            showTimes: 1,
+          $group: {
+            _id: "$cinemaId",
+            showTimes: { $push: "$$ROOT" },
+            cinema: { $addToSet: "$cinema" },
           },
+        },
+        {
+          $unwind: "$cinema",
         },
       ])
       .toArray();
 
-      console.log(pipeline, "????, showtime model");
+    console.log(`Fetched ${pipeline.length} showtimes.`);
+    console.log(pipeline);
+
     return pipeline as IShowTime[];
   }
 }
